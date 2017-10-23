a963204132b1e039e23b81edbc491116eb37846e
Chris Watterston <hello@chriswatterston.com>
2017-10-23 15:27:41 +0100

    - Removed the date and type from the post listing

--- application/blocks/page_list/templates/chris-watterston-inpage/view.php
+++ application/blocks/page_list/templates/chris-watterston-inpage/view.php
@@ -17,44 +17,29 @@
 		$description = $page->getCollectionDescription();
 		$description = $controller->truncateSummaries ? $th->wordSafeShortText($description, $controller->truncateChars) : $description;
 		$description = $th->entities($description);
-        $date = $page->getCollectionDatePublicObject()->format('jS F Y');
-        $img = false;
+    $img = false;
 
-		$posttypetitle = $page->getAttribute('post_topics');
-		$posttypetitlefirst = $posttypetitle[0]->gettreeNodeName();
-
-		$posttyperesult = "post-type-full";
-		$micropost = $page->getAttribute('micro_post');
-		$microposttag = "";
-		if ($micropost) {
-			$posttyperesult = "post-type-micro";
-			$microposttag = "Micro Post <span class='type-closetag'></span> ";
-
-		}
-	    if ($displayThumbnail) {
-		    $img = $page->getAttribute('thumbnail');
-	    }
+    if ($displayThumbnail) {
+	    $img = $page->getAttribute('thumbnail');
+    }
 		if ($img) {
 			$thumb = $ih->getThumbnail($img, 600, 320, array('crop'=>true, 'quality'=>40));
 			$tagSrc = $thumb->src;
 		}
-        $i++;
+    $i++;
 		?>
-		<div class="col newsfeed-entry entry-<?php echo $i; ?> <?php echo $posttyperesult; ?>">
+		<div class="col newsfeed-entry entry-<?php echo $i; ?>">
 			<div class="news-img" style="background-image:url('<?php echo $tagSrc; ?>');"></div>
 			<div class="news-content-wrap">
 				<div class="news-content">
-		            <?php if ($includeName): ?>
-		            	<h2 class="colour-grey type-semibold"><?php echo $title ?></h2>
-		            <?php endif; ?>
-		            <p class="date-tag colour-grey type-semibold type-uppercase">
-			            <?php if ($includeDate) { echo $date; } ?> <span class="type-closetag"></span> <?php echo $microposttag, $posttypetitlefirst, " Post"; ?>
-		            </p>
+          <?php if ($includeName): ?>
+          	<h2 class="colour-grey type-semibold"><?php echo $title ?></h2>
+          <?php endif; ?>
 					<p class="description colour-grey">
 						<?php if ($includeDescription): ?>
 							<?php echo $description ?>
 						<?php endif; ?>
-		            </p>
+					</p>
 				</div>
 			</div>
 			<a href="<?php echo $url ?>" target="_self" title="Continue reading, <?php echo $title ?>" class="hidden"></a>

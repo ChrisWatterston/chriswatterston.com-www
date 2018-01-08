4390922f51c6344506d3342ab15128c4d6607a7e
Chris Watterston <hello@chriswatterston.com>
2018-01-08 21:44:45 +0000

    - Deleted old pages - Fixed a few issues

--- application/config/database.php
+++ application/config/database.php
@@ -1,15 +1,10 @@
 <?php
-
 return [
     'default-connection' => 'concrete',
     'connections' => [
         'concrete' => [
             'driver' => 'c5_pdo_mysql',
-<<<<<<< HEAD
             'server' => 'phpmyadmin.dev-local',
-=======
-            'server' => 'phpmyadmin.local',
->>>>>>> 2433b622b38d1ee8a019b3bbc5f8e84d0974fd78
             'database' => 'chriswatterston_www',
             'username' => 'root',
             'password' => 'root',

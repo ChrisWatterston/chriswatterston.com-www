3fbd38542a4ae6f770cc5a61ca9bfbfa88d4634e
Chris Watterston <chris@chriswatterston.com>
2023-01-18 16:23:49 +0000

    Disable of GA, leaving just Cabin

--- config/analytics.global.php
+++ config/analytics.global.php
@@ -1,17 +1,17 @@
 <?php
 if (getenv('ENVIRONMENT_STATE') === 'production') {
-    echo '
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-V9KJ4D564B"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
+    // echo '
+    //     <script async src="https://www.googletagmanager.com/gtag/js?id=G-V9KJ4D564B"></script>
+    //     <script>
+    //         window.dataLayer = window.dataLayer || [];
 
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag(\'js\', new Date());
+    //         function gtag() {
+    //             dataLayer.push(arguments);
+    //         }
+    //         gtag(\'js\', new Date());
 
-            gtag(\'config\', \'G-V9KJ4D564B\');
-        </script>
-    ';
+    //         gtag(\'config\', \'G-V9KJ4D564B\');
+    //     </script>
+    // ';
     echo '<script async defer src="https://scripts.withcabin.com/hello.js"></script>';
 }

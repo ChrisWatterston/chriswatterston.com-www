1de0dbedbd6d3046a7271c7fb8beb1c7c5c9daae
Chris Watterston <chris@chriswatterston.com>
2023-02-23 16:54:12 +0000

    Changed from 3rd person to first

--- side-passion-projects.php
+++ side-passion-projects.php
@@ -28,8 +28,8 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                         </div>
                         <div class="_sub-detail">
                             <p>Side projects and passion projects, either way you view them, both can (and normally will) inspire an onlooker. They are projects that push you to explore your inner creativity and personality.</p>
-                            <p>Chris is passionate about creativity, the internet of technologies, and starting new and exciting things. Constantly being creative inspires him to tap into something bigger which he may not be able to do so in a 9-to-5. Something which he can think outside of the box with, potentially exploring with no boundaries.</p>
-                            <p>Below are some of the side projects and passion projects Chris has entertained over the years. Some are exciting and some are worthy, but others are questionable. 🫣</p>
+                            <p>I'm passionate about creativity, the internet of technologies, and starting new and exciting things. Constantly being creative inspires me to tap into something bigger which may not be able to do so in a 9-to-5. Something which I can think outside of the box with, potentially exploring with no boundaries.</p>
+                            <p>Below are some of the side projects and passion projects I've entertained over the years. Some are exciting and some are worthy, but others are questionable. 🫣</p>
                         </div>
                     </section>
                     <section class="<?= $globalPrefix; ?>-grid-container _column-about _about-body-experience">
@@ -44,8 +44,8 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                     <p class="_post-date -active">Active</p>
                                 </div>
                             </div>
-                            <p>While Chris worked for Suntory, he became part of a team which rolled out the project management software Wrike. It took a few years for all business planning and projects to migrate into Wrike, but during that time Chris became very familiar with the platform.</p>
-                            <p>Once he became familiar with the features Wrike offered, it quickly became clear to him that there was something missing. He recognised a hole within Wrike's large audience which hadn't been tailored for. This is what sparked the idea of Wrikified.</p>
+                            <p>While working at Suntory, I became part of a team which rolled out the project management software Wrike. It took a few years for all business planning and projects to migrate into Wrike, but during that time I became very familiar with the platform.</p>
+                            <p>After I became familiar with the features Wrike offered, it quickly became clear to me that there was something missing. I recognised a hole within Wrike's large audience which hadn't been tailored for. This is what sparked the idea of Wrikified.</p>
                             <p>Wrikified is a simple tool to use alongside Wrike. It was built for production teams, removing the noise and clutter from Wrike. It enabled managers to continue to use the most powerful work management software, but have it simplified for their production teams.</p>
                             <!-- <p>
                                 <a href="https://github.com/Wrikified" target="_blank" title="Visit the Wrikified Github Profile" class="_about-social-icons">
@@ -63,7 +63,7 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                     <p class="_post-date -active">Active</p>
                                 </div>
                             </div>
-                            <p>This is a side project that Chris is very passionate about. It's based on an idea which he's had for a number of years, but has only now started putting pen to paper.</p>
+                            <p>This is a side project that I am very passionate about. It's based on an idea which I've had for a number of years, but have only now started putting pen to paper.</p>
                             <p>More details will be shared at a later date.</p>
 
                             <div class="<?= $globalPrefix; ?>-grid-container _stat-headline">
@@ -74,10 +74,10 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                 </div>
                             </div>
                             <p>SweepScore is a bespoke designed and built football sweepstake web application. The app was built around the realtime data provided by Sportmonks, with an added feature enabling users to connect their Strava account for extra health and wellbeing points.</p>
-                            <p>Chris built and deployed the Beta release to users back in 2021, which fell in alignment with the Men's Euros Football Championship. The platform quickly became a huge success amongst users, enabling them to compete and tease one another on becoming top of the leader board.</p>
-                            <p>Chris was fortunate enough to have Suntory Beverage & Food Europe as the first Beta users.</p>
-                            <p>During this time Chris learnt a lot about how the app was being accepted, used, collecting user feedback, and obviously fixed an array of bugs. With all this data and insight, Chris had a dream to make the idea into something special.</p>
-                            <p>Since the early Beta and analysing the data, the idea has since been rebuilt and renamed into something unique with bigger intentions. Chris has a dream to make the project available for others to use, or possibly even open-source before the close of 2023. 😉</p>
+                            <p>I built and deployed the Beta release to users back in 2021, which fell in alignment with the Men's Euros Football Championship. The platform quickly became a huge success amongst users, enabling them to compete and tease one another on becoming top of the leader board.</p>
+                            <p>I was fortunate enough to have Suntory Beverage & Food Europe as the first Beta users.</p>
+                            <p>During this time, I learnt a lot about how the app was being accepted, used, collecting user feedback, and obviously fixed an array of bugs. With all this data and insight, I had a dream to make the idea into something special.</p>
+                            <p>Since the early Beta and analysing the data, the idea has since been rebuilt and grown into something unique, with bigger intentions. I've had a dream to make the project available for others to use, or possibly even open-source before the close of 2023. Let's see what happens. 😉</p>
 
                             <div class="<?= $globalPrefix; ?>-grid-container _stat-headline">
                                 <div class="_icon" style="background-image: url(/dist/images/brands/logo-sideproject-skoolping-app.jpg);"></div>
@@ -87,8 +87,8 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                 </div>
                             </div>
                             <p>The SKOOLping mission was to bring modern technology and high quality design to the educational comms space. Enabling a rich and seamless communication function for the teacher-to-parent community.</p>
-                            <p>Chris has a working background in the education sector, therefore had seen first hand how edu. is left behind with design and technology.</p>
-                            <p>SKOOLping was going to be a game changer. Ensuring a beautifully designed and reliable service was available for all types of education pillars. Regular updates would ensure SKOOLping was aligned with todays tech, all managed by the user through a simple UI.</p>
+                            <p>I had a working background in the education sector, therefore had seen first hand how the education sector was always left behind with design and technology.</p>
+                            <p>SKOOLping was going to become a game changer. Ensuring a beautifully designed and reliable service was available for all types of education pillars. Regular updates would ensure SKOOLping was aligned with todays tech, all managed by the user through a simple UI.</p>
                             <p>This was really exciting for education providers!</p>
 
                             <div class="<?= $globalPrefix; ?>-grid-container _stat-headline">
@@ -98,9 +98,9 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                     <p class="_post-date -inactive">Inactive</p>
                                 </div>
                             </div>
-                            <p>Chris became a first time dad in 2010. This is when the idea of a web based platform called My Childs Diary started.</p>
+                            <p>Becoming a first time dad in 2010 and having a curious creative mind, an idea sparked of building a web based platform called My Childs Diary.</p>
                             <p>The over arching idea was right in the name. This would become a place for parents to record and capture their Childs life as they unfolded. With premium features, which would take all the recorded memories and put them into both a printed and digital book for the user to cherish.</p>
-                            <p>Unfortunately the build was much to slow, due to the juggling of parenthood and his 9-to-5 day job. This inevitability meant a competitor beat him to market, little over 2 years later.</p>
+                            <p>Unfortunately the build was much to slow, due to the juggling of parenthood and my 9-to-5 day job. This inevitability meant a competitor beat me to market, little over 2 years later.</p>
 
                             <div class="<?= $globalPrefix; ?>-grid-container _stat-headline">
                                 <div class="_icon" style="background-image: url(/dist/images/brands/logo-sideproject-fancyeat-app.jpg);"></div>
@@ -109,8 +109,8 @@ $localMetaKeywords = 'Side Project, Passion Projects, Product Designer Projects,
                                     <p class="_post-date -inactive">Inactive</p>
                                 </div>
                             </div>
-                            <p>After the failure of My Childs Diary, Chris aspired to put new ideas on the table. One of his other early ideas in 2011 was to create a place for users to easily and quickly locate the food they loved and craved.</p>
-                            <p>Looking back at the idea, it could be imagined as another Just Eat or Deliveroo- but you'd need to get off your sofa to collect (or eat in the restaurant).</p>
+                            <p>After the failure of My Childs Diary, I aspired to put new ideas on the table. One of my other early ideas in 2011, was to create a place for users to easily and quickly locate the food they loved and craved.</p>
+                            <p>Looking back at the idea, it could be imagined as another Just Eat or Deliveroo- you could order your food online, but you'd need to get off your sofa to collect (or eat in the restaurant).</p>
 
                         </div>
                     </section>
